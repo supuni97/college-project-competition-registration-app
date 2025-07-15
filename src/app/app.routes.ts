84b622f74b997186ad9fb4b7f6fae7e63b254ba7
supuni97 <sitharabandara1997@gmail.com>
2025-07-15 13:22:26 +0530

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { SubmitProjectComponent } from './pages/submit-project/submit-project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'students',
+    component: StudentsComponent,
+  },
+  {
+    path: 'submitProject',
+    component: SubmitProjectComponent,
+  },
+  {
+    path: 'all-projects',
+    component: ProjectsComponent,
+  },
+];
