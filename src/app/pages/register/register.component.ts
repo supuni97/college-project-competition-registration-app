8c45a4576cd8abd6835567ffad4dd8e09b8b4728
supuni97 <sitharabandara1997@gmail.com>
2025-07-18 19:17:11 +0530

    Completed register ui and integrated with the api

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  showPassword = false;
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  registerObj: any = {
+    fullName: '',
+    email: '',
+    password: '',
+    collegeName: '',
+    role: '',
+  };
+
+  http = inject(HttpClient);
+
+  onRegister() {
+    debugger;
+    this.http
+      .post(
+        'https://api.freeprojectapi.com/api/ProjectCompetition/register',
+        this.registerObj
+      )
+      .subscribe((res: any) => {
+        alert('Student registration success');
+      });
+  }
 }
