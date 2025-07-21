d872c88e2bbb94e07c0bf985a4c2b4d9dd680e22
supuni97 <sitharabandara1997@gmail.com>
2025-07-21 13:11:31 +0530

    Fetched competition date to home featured section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CompetitionService } from '../../sevices/competition.service';
+import { Observable } from 'rxjs';
+import { CompetitionModel } from '../../model/competition.model';
+import { AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AsyncPipe, DatePipe],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  src = inject(CompetitionService);
 
+  competitionList$: Observable<CompetitionModel[]> = new Observable<
+    CompetitionModel[]
+  >();
+
+  constructor() {
+    this.competitionList$ = this.src.getCompetition();
+  }
 }
